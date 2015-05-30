8275fb949c65e8b7f23a7d94bed19d6e16ced4f7
Nicolas Schmidt <nfschmidt@gmail.com>
2015-05-30 12:55:58 -0300

    Agregue not nulls

--- migracion/Migracion.sql
+++ migracion/Migracion.sql
@@ -13,8 +13,8 @@ USE GD1C2015
 /****************************************************************/
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Rol](
 	[Id_Rol][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[Rol_Nombre] [varchar](255),
-	[Habilitado][char](1)
+	[Rol_Nombre] [varchar](255) NOT NULL,
+	[Habilitado][char](1) NOT NULL
 )
 
 INSERT INTO [LA_MAQUINA_DE_HUMO].Rol Values('Administrador','s')
@@ -27,7 +27,7 @@ GO
 /****************************************************************/
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Funcionalidad](
 	[Id_Funcionalidad][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[Func_Nombre] [varchar](255),
+	[Func_Nombre] [varchar](255) NOT NULL
 )
 
 INSERT INTO [LA_MAQUINA_DE_HUMO].Funcionalidad Values('ABM de Rol')
@@ -58,10 +58,14 @@ CREATE TABLE [LA_MAQUINA_DE_HUMO].[Rol_Funcionalidad](
 )
 
 INSERT INTO [LA_MAQUINA_DE_HUMO].Rol_Funcionalidad(Id_Rol , Id_Funcionalidad) 
-select 1,Id_Funcionalidad from [LA_MAQUINA_DE_HUMO].Funcionalidad
+	SELECT 1,Id_Funcionalidad
+	FROM [LA_MAQUINA_DE_HUMO].Funcionalidad
 
 INSERT INTO [LA_MAQUINA_DE_HUMO].Rol_Funcionalidad(Id_Rol , Id_Funcionalidad) 
-select 2,Id_Funcionalidad from [LA_MAQUINA_DE_HUMO].Funcionalidad WHERE Id_Funcionalidad IN (1,2,3,4,5,6,7) /** Cargar con las correspondientes funcionalidades **/
+	SELECT 2,Id_Funcionalidad
+	FROM [LA_MAQUINA_DE_HUMO].Funcionalidad
+	WHERE Id_Funcionalidad IN (1,2,3,4,5,6,7) -- Cargar con las correspondientes funcionalidades
+
 GO
 
 /****************************************************************/
@@ -69,7 +73,7 @@ GO
 /****************************************************************/
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Pais](
 	[Pais_Codigo][numeric](18,0) PRIMARY KEY,
-	[Pais_Desc][varchar](250)
+	[Pais_Desc][varchar](250) NOT NULL
 )
 
 INSERT INTO [LA_MAQUINA_DE_HUMO].[Pais] (
@@ -79,10 +83,10 @@ INSERT INTO [LA_MAQUINA_DE_HUMO].[Pais] (
 SELECT DISTINCT Cuenta_Pais_Codigo, Cuenta_Pais_Desc
 	FROM gd_esquema.Maestra
 UNION
-SELECT DISTINCT Cli_Pais_Codigo, Cli_Pais_Desc
-	FROM gd_esquema.Maestra
-	WHERE Cli_Pais_Codigo NOT IN (SELECT Cuenta_Pais_Codigo
-									FROM gd_esquema.Maestra)
+	SELECT DISTINCT Cli_Pais_Codigo, Cli_Pais_Desc
+		FROM gd_esquema.Maestra
+		WHERE Cli_Pais_Codigo NOT IN (SELECT Cuenta_Pais_Codigo
+										FROM gd_esquema.Maestra)
 
 /*
 	SELECT DISTINCT Cli_Pais_Codigo, Cli_Pais_Desc
@@ -96,7 +100,7 @@ GO
 /****************************************************************/
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Documento](
 	[Doc_Codigo][numeric](18,0) PRIMARY KEY,
-	[Doc_Desc][varchar](255)
+	[Doc_Desc][varchar](255) NOT NULL
 )
 
 INSERT INTO [LA_MAQUINA_DE_HUMO].[Documento](
@@ -113,12 +117,12 @@ GO
 /****************************************************************/
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Usuario](
 	[Id_Usuario][int] IDENTITY (1,1) PRIMARY KEY,
-	[Cantidad_Intentos_Fallidos][int],
-	[Username][varchar](255),
+	[Cantidad_Intentos_Fallidos][int] NOT NULL,
+	[Username][varchar](255) NOT NULL,
 	[Password][varchar](255),
-	[Habilitado][char](1),
-	[Fecha_Creacion][datetime],
-	[Fecha_Ultima_Modificacion][datetime],
+	[Habilitado][char](1) NOT NULL,
+	[Fecha_Creacion][datetime] NOT NULL,
+	[Fecha_Ultima_Modificacion][datetime] NOT NULL,
 	[Pregunta_Secreta][varchar](255),
 	[Respuesta_Secreta][varchar](255)
 )
@@ -136,7 +140,7 @@ INSERT INTO [LA_MAQUINA_DE_HUMO].Usuario(
 	SELECT DISTINCT
 		0,
 		'usuario' + RIGHT(CONVERT(varchar(18),Cli_Nro_Doc),18),
-		'6b3a55e0261b0304143f805a24924d0c1c44524821305f31d9277843b8a10f4e',
+		'5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', --SHA256 de "password"
 		's',
 		GETDATE(),
 		GETDATE(),
@@ -152,20 +156,20 @@ GO
 /****************************************************************/
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Clientes](
 	[Id_Cliente] [int] IDENTITY(1,1) NOT NULL,
-	[Cli_Nombre] [varchar](255),
-	[Cli_Apellido] [varchar](255),
-	[Cli_Nro_Doc] [numeric](18,0),
+	[Cli_Nombre] [varchar](255) NOT NULL,
+	[Cli_Apellido] [varchar](255) NOT NULL,
+	[Cli_Nro_Doc] [numeric](18,0) UNIQUE NOT NULL,
 	[Cli_Tipo_Doc_Cod] [numeric] (18,0) FOREIGN KEY REFERENCES [LA_MAQUINA_DE_HUMO].Documento(Doc_Codigo),
-	[Cli_Mail] [varchar](255),
-	[Cli_Pais_Codigo] [numeric] (18,0),
-	[Cli_Dom_Nro][numeric](18,0),
-	[Cli_Dom_Calle] [varchar](255),
+	[Cli_Mail] [varchar](255) UNIQUE NOT NULL,
+	[Cli_Pais_Codigo] [numeric] (18,0) FOREIGN KEY REFERENCES [LA_MAQUINA_DE_HUMO].Pais(Pais_Codigo),
+	[Cli_Dom_Nro][numeric](18,0) NOT NULL,
+	[Cli_Dom_Calle] [varchar](255) NOT NULL,
 	[Cli_Dom_Piso] [numeric](18,0),
 	[Cli_Dom_Depto] [varchar](10),
-	[Cli_Dom_Localidad][varchar](255),
+	[Cli_Dom_Localidad][varchar](255), /* Hay localidades que son NULL, ojo aca */
 	[Cli_Nacionalidad_Codigo][numeric](18,0) FOREIGN KEY REFERENCES [LA_MAQUINA_DE_HUMO].Pais(Pais_Codigo), /** Es el mismo para [Cli_Pais_Codigo] **/
-	[Cli_Fecha_Nac][datetime],
-	[Cli_Habilitado][char](1),
+	[Cli_Fecha_Nac][datetime] NOT NULL,
+	[Cli_Habilitado][char](1) NOT NULL,
 	[Id_Usuario][int] FOREIGN KEY REFERENCES [LA_MAQUINA_DE_HUMO].Usuario(Id_usuario) /** FK a Usuario **/
  )
 GO
@@ -215,7 +219,7 @@ GO
 CREATE TABLE [LA_MAQUINA_DE_HUMO].[Auditoria](
 	[Id_Auditoria][int] IDENTITY (1,1) PRIMARY KEY,
 	[Id_Usuario][int] FOREIGN KEY REFERENCES [LA_MAQUINA_DE_HUMO].Usuario(Id_Usuario),
-	[Fecha][datetime],
-	[Resultado][varchar](255),
-	[Nro_Intento][int]
+	[Fecha][datetime] NOT NULL,
+	[Resultado][varchar](255) NOT NULL,
+	[Nro_Intento][int] NOT NULL
 )
